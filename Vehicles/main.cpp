0d22049ff7fd1760513f02d64f9415144600c31c
Leonardo Rodriguez <leonardorm7@hotmail.com>
2022-09-08 21:09:48 -0500

    adding protected inheritance

--- Vehicles/main.cpp
+++ Vehicles/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Turism.h"
 #include "Sport.h"
+#include "Furgoneta.h"
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -16,7 +17,14 @@ int main(int argc, char** argv) {
 	Sport *d1 = new Sport("Audi","Yellow","KP98",10);
 	cout<<"cylinder_capacity: "<<d1->getcylinder()<<endl;
 	cout<<"Brand: "<<d1->reportBrand()<<endl;
+	
+	Furgoneta *f1=new Furgoneta("Kia","Green","AS12",1200);
+	
+	cout<<"The freight is: "<<f1->getCarga()<<endl;
+	cout<<"Color: "<<f1->reportarcolor()<<endl;
+	
 	delete t1;
 	delete d1;
+	delete f1;
 	return 0;
 }
